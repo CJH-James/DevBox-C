b8489afaf5fcfd7adc8f5f2cc65872403b5354ff
CJH-James <qaz75203code@gmail.com>
2025-03-26 00:27:38 +0800

    [LOG] Refine the log function
    [Description]
    1. Refine the eTimestamp name
    2. Fill the comment for doxygen
    3. Format the code

--- include/log/header/log.h
+++ include/log/header/log.h
@@ -3,28 +3,26 @@
 
 #include <stdio.h>
 
-
 // Define ANSI color codes
 #define GRAY   "\033[1;30m"
 #define RED    "\033[1;31m"
 #define GREEN  "\033[1;32m"
-#define YELLOW  "\033[1;33m"
+#define YELLOW "\033[1;33m"
 #define BLUE   "\033[1;34m"
 #define Cyan   "\033[1;36m"
 #define RESET  "\033[0m"
 
-
 typedef enum
 {
-    NO_TIMESTAMP = 0,
-    WITH_TIMESTAMP = 1
+    NONE           = 0, // none
+    WITH_TIMESTAMP = 1  // with timestamp
 } eTimestamp;
 
-
-#define DEBUG(fmt, ...)\
-        do { \
-            fprintf(stderr, YELLOW"[DEBUG]"RESET"%s:%d %s():" fmt "\n", __FILE_NAME__, __LINE__, __func__, ##__VA_ARGS__); \
-        } while (0)
+#define DEBUG(fmt, ...)                                                                                                   \
+    do                                                                                                                    \
+    {                                                                                                                     \
+        fprintf(stderr, YELLOW "[DEBUG] " RESET "%s:%d %s():" fmt "\n", __FILE_NAME__, __LINE__, __func__, ##__VA_ARGS__); \
+    } while (0)
 
 // Function declarations
 void log_message(const char *level, const char *color, const char *message, eTimestamp timeFlag);
--- include/log/src/log.c
+++ include/log/src/log.c
@@ -6,9 +6,9 @@
 
 /**
  * @brief Get the current timestamp in HH:MM:SS.mmm format
- * 
- * @param buffer 
- * @param size 
+ *
+ * @param[out] timeStr The buffer to store the formatted
+ * @param[in]  size    The size of the buffer (should be at least 13 bytes).
  */
 void get_timestamp(char *timeStr, size_t size)
 {
@@ -19,7 +19,14 @@ void get_timestamp(char *timeStr, size_t size)
     snprintf(timeStr + 8, size - 8, ".%03ld", tv.tv_usec / 1000);
 }
 
-// Generic logging function
+/**
+ * @brief General function for printing log message
+ *
+ * @param[in] level    The pre-fix string
+ * @param[in] color    The pre-fix string color
+ * @param[in] message  The log message
+ * @param[in] timeFlag The timestamp flag (eTimestamp)
+ */
 void log_message(const char *level, const char *color, const char *message, eTimestamp timeFlag)
 {
     char timestamp[16] = "";
@@ -29,22 +36,37 @@ void log_message(const char *level, const char *color, const char *message, eTim
         printf("|%s|", timestamp);
     }
 
-    printf("%s%-4s"RESET"│ %s\n", color, level, message);
+    printf("%s%-4s" RESET "│ %s\n", color, level, message);
 }
 
-// INFO logging function
+/**
+ * @brief The [INFO] log message
+ *
+ * @param[in] message  The log message
+ * @param[in] timeFlag The timestamp flag (eTimestamp)
+ */
 void log_info(const char *message, eTimestamp timeFlag)
 {
     log_message("INFO", BLUE, message, timeFlag);
 }
 
-// WARN logging function
+/**
+ * @brief The [WARN] log message
+ *
+ * @param[in] message  The log message
+ * @param[in] timeFlag The timestamp flag (eTimestamp)
+ */
 void log_warn(const char *message, eTimestamp timeFlag)
 {
     log_message("WARN", GRAY, message, timeFlag);
 }
 
-// ERR logging function
+/**
+ * @brief The [ERR] log message
+ *
+ * @param[in] message  The log message
+ * @param[in] timeFlag The timestamp flag (eTimestamp)
+ */
 void log_err(const char *message, eTimestamp timeFlag)
 {
     log_message("ERR", RED, message, timeFlag);
