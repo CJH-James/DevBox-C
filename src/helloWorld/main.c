59bd354a1e2d4fde76557287bed3a55e02f96f30
CJH-James <qaz75203code@gmail.com>
2025-03-26 00:41:06 +0800

    [HelloWorld] Initial the 1st program for testing
    [Description]
    1. Test log function work
    2. Create the template for getopt()

--- src/helloWorld/main.c
+++ src/helloWorld/main.c
@@ -1,21 +1,57 @@
+#include <stdio.h>
+#include <unistd.h> /* getopt*/
 
 #include "log.h"
-#include "operateArgument.h"
 
-int main (int argc, char *argv[])
+static void printHelp(char *argv[])
 {
-    sArgument argument = {0};
+    printf("\n");
+    printf("Usage: %s -p N [-oString]\n", argv[0]);
+    printf("\n");
+    printf("Arguments:\n");
+    printf("    -p N       (necessary argument)\n");
+    printf("    -oString   (optional argument)\n");
+}
 
-    printArgument(argument);
+int main(int argc, char *argv[])
+{
+    int  opt      = 0;
+    char msg[128] = {0};
 
-    log_info("hi", NO_TIMESTAMP);
-    log_warn("hi", NO_TIMESTAMP);
-    log_err("hi", NO_TIMESTAMP);
-    log_info("hi", WITH_TIMESTAMP);
-    log_warn("hi", WITH_TIMESTAMP);
-    log_err("hi", WITH_TIMESTAMP);
+    // at least 1 arg
+    if (argc == 1)
+    {
+        printHelp(argv);
+        return 0;
+    }
 
-    DEBUG("test");
+    sprintf(msg, "Process Idx = %d, error=%d", optind, opterr);
+    log_info(msg, NONE);
+    log_warn(msg, NONE);
+    log_err(msg, NONE);
+    log_info(msg, WITH_TIMESTAMP);
+    log_warn(msg, WITH_TIMESTAMP);
+    log_err(msg, WITH_TIMESTAMP);
+
+    while ((opt = getopt(argc, argv, "hp:o::")) != -1)
+    {
+        // h   -> no arg
+        // p:  -> 1 arg
+        // o:: -> optional arg
+        switch (opt)
+        {
+            case 'p':
+                DEBUG("optarg = %s", optarg);
+                break;
+            case 'o':
+                DEBUG("optarg = %s", optarg);
+                break;
+            case 'h':
+            default:
+                printHelp(argv);
+                break;
+        }
+    }
 
     return 0;
 }
